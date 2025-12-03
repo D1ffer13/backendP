a826d4ab045f8167388084da183fb854c561a164
D1ffer13 <gaga888nana@gmail.com>
2025-12-03 12:40:57 +0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'mysql.railway.internal',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'education_crm',
+  password: process.env.DB_PASSWORD || 'xtFBfIsguBZFCHUZtkGoaAjVaPHhZKoP',
+  database: process.env.DB_NAME || 'railway',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
