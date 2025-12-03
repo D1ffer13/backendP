0cde2908a87d40bd7e945a28baed4b6bb42daaa6
D1ffer13 <gaga888nana@gmail.com>
2025-12-03 12:49:30 +0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,15 @@
-
 // backend/config/db.js
-
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'mysql.railway.internal',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'xtFBfIsguBZFCHUZtkGoaAjVaPHhZKoP',
-  database: process.env.DB_NAME || 'railway',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  // ВАЖНО: отключаем автоматическую конвертацию дат в UTC
-  timezone: '+00:00',
-  dateStrings: true // Возвращаем даты как строки
 });
 
-// Промисифицируем pool
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = pool;
